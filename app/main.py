d017a86a2ef9df778e29179d85a33a076cfad62a
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-02-18 15:54:25 -0500

    fix: set correct datetime, import models

--- app/main.py
+++ app/main.py
@@ -4,6 +4,14 @@ from app.core.lifespan import lifespan
 from app.db.database import Base, engine
 from app.routers import files
 
+import importlib
+import pkgutil
+import app.models
+
+for module_info in pkgutil.iter_modules(app.models.__path__, app.models.__name__ + "."):
+    _ = importlib.import_module(module_info.name)
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(lifespan=lifespan)
--- app/models/document.py
+++ app/models/document.py
@@ -1,9 +1,10 @@
 # pyright: reportImportCycles=false
+from datetime import datetime
 import enum
 from typing import TYPE_CHECKING
 from uuid import UUID
 
-from sqlalchemy import DateTime, Enum, ForeignKey, func
+from sqlalchemy import Enum, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.database import Base
 
@@ -28,7 +29,9 @@ class Document(Base):
     file_type: Mapped[FileType] = mapped_column(Enum(FileType), nullable=False)
     size: Mapped[int] = mapped_column(nullable=False)
     s3_location: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(default=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), nullable=False
+    )
 
     user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
 

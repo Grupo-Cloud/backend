6cc3001e69557636e61b054ec83aae54cf78397b
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-02-26 17:41:20 -0500

    feat: endpoints and service functions for user

--- app/services/user.py
+++ app/services/user.py
@@ -5,7 +5,10 @@ from pydantic import EmailStr
 from sqlalchemy import insert, select
 from sqlalchemy.orm import Session
 
+from app.exceptions.user import UserNotFoundException
 from app.models.user import User
+from app.models.document import Document
+from app.models.chat import Chat
 
 
 @final
@@ -30,6 +33,20 @@ class UserService:
         )
         _ = db.execute(statement)
         db.commit()
+
+    def get_documents_from_user(self, db: Session, user_id: UUID) -> list[Document]:
+        statement = select(User).filter_by(id=user_id)
+        user = db.execute(statement).scalar_one_or_none()
+        if not user:
+            raise UserNotFoundException(f"User with id {user_id} could not be found")
+        return user.documents
+
+    def get_chats_from_user(self, db: Session, user_id: UUID) -> list[Chat]:
+        statement = select(User).filter_by(id=user_id)
+        user = db.execute(statement).scalar_one_or_none()
+        if not user:
+            raise UserNotFoundException(f"User with id {user_id} could not be found")
+        return user.chats
 
 
 service = UserService()
